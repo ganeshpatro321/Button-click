bc31f7cca7d484f1ae9e17cadad85fc9288e99e1
ganeshpatro321 <ganeshpatro321@gmail.com>
2018-10-03 00:34:36 +0530

    Disconnect Event Driven along with number of users

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ const io = socketIO(server);
 app.use(express.static(path.resolve(__dirname, 'public')));
 
 let clicks = 0;
+let users = 0
 
 io.on('connection', function(socket){
+  console.log("User Connected")
+  users++;
+  console.log(users)
   socket.on('click', function(){
     console.log('Clicked');
+    io.emit('userLogged', users)
     io.emit('clickChanged', ++clicks);
   });
+  socket.on("disconnect", function(){
+    users--;
+    clicks = 0;
+    console.log("User Disconnected");
+    console.log(users)
+  })
 });
+
+
 
 server.listen(3000, () => {
 	console.log('listening on *:3000');
